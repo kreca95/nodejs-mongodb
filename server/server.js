fc8bee2e6d0d058ca00315802ca5994d39cac457
Kresimir Sutalo <kresimir095@gmail.com>
2019-05-09 19:04:51 +0200

    heroku stuff

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var { Todo } = require("./models/todo");
 var { User } = require("./models/user")
 
 var app = express();
+const port=process.env.port || 3000;
+
 app.use(bodyParser.json());
 
 
@@ -49,8 +51,8 @@ app.get("/todos/:id", (req, res) => {
     }
 })
 
-app.listen(3000, () => {
-    console.log("started on port 3000");
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {
